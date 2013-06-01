2714e406abe0eb0cfdd6443773eec376d8844670
Code Ass <aycabta@gmail.com>
2013-06-01 13:44:24 +0900

    created Web UI that is named suibun

---
+++ suibun/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suibun::Application.config.secret_token = 'ac8eb277ce0563f673748213329ed4d26c0221e2aebd3cec94feede8d8a6562727070abdacdfd625bfa2299d057d67f8bc7d4eba311a62c16db9a306bc224b98'
