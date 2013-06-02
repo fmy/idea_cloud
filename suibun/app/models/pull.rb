044947b7f8d3bda752dc02d2b1d062fb6c9e9259
Ubuntu <aycabta@gmail.com>
2013-06-02 05:02:41 +0000

    fixed restart service command

--- suibun/app/models/pull.rb
+++ suibun/app/models/pull.rb
@@ -1,6 +1,6 @@
 class Pull
   def self.pull_and_restart
-    system("cd /home/ubuntu/idea_cloud && git pull && sudo suibun stop && sudo suibun start")
+    system("cd /home/ubuntu/idea_cloud && git pull && sudo service suibun stop && sudo service suibun start")
   end
 end
 
